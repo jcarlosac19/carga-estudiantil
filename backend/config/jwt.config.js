bbe50a9566608cd2b671ff871c77e0e823dc2640
Isaac Suazo <isaacsuazo7@gmail.com>
2023-02-14 22:46:58 -0600

    + changes struct

--- backend/config/jwt.config.js
+++
@@ -1,6 +0,0 @@
-// Application configuration.
-const config = module.exports;
-config.keys = {
-    secret: process.env.JWT_SECRET || 'MySecretHash',
-    expiresIn: process.env.JWT_EXPIRES_IN ||'24h'
-};
--- backend/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const controller = require("../controllers/user.controller");
-const token = require('../middleware/jwt.auth');
-const app = Router();
-
-app.get("/:id", token.verifyToken, controller.findUserById);
-
-
-module.exports = app;
