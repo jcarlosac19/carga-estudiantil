065369dab825bbb3e44c501e9cbf2cfebc54586c
Juan Alvarez <jcarlosac19@gmail.com>
2023-02-24 21:05:55 -0600

    Revisando propiedades de la carga.

--- controllers/carga.academica.controller.js
+++ controllers/carga.academica.controller.js
@@ -7,6 +7,20 @@ exports.crearCargaAcademica = async(req, res) => {
     creadoPor = req.user.user_id;
     records = req.body;
 
+    const {codigoMateria, nombreMateria, codigoCarrera, nombreCarrera, requisitoUno, requisitoDos, anio, requisitoUnidadesValorativas, unidadesValorativas, plan, requerimientoIndiceGraduacion } = req.body[0];
+
+    if(!codigoMateria) return res.status(400).send({message: "Falta el codigo de la materia."});
+    if(!nombreMateria) return res.status(400).send({message: "Falta el nombre de la materia."});
+    if(!codigoCarrera) return res.status(400).send({message: "Falta el codigo de la carrera."});
+    if(!nombreCarrera) return res.status(400).send({message: "Falta el nombre de la materia."});
+    if(!requisitoUno) return res.status(400).send({message: "Falta el requisito uno."});
+    if(!requisitoDos) return res.status(400).send({message: "Falta el requisito dos."});
+    if(!anio) return res.status(400).send({message: "Falta el año de la carga."});
+    if(!unidadesValorativas) return res.status(400).send({message: "Falta las unidades valorativas."});
+    if(!requisitoUnidadesValorativas) return res.status(400).send({message: "Falta el requisito de las unidades valorativas."});
+    if(!plan) return res.status(400).send({message: "Falta plan de la carga."});
+    if(!requerimientoIndiceGraduacion) return res.status(400).send({message: "Falta el requisito de indice de graduacion."});
+    
     await cargaAcademicaVersion.updateMany({},{estaActiva: false }, {new: true});
 
     cargaVersion = new cargaAcademicaVersion({
@@ -16,7 +30,7 @@ exports.crearCargaAcademica = async(req, res) => {
     });
 
     await cargaVersion.save(async function(err){
-        if(err) return res.status(401).send("Hubo un error.");
+        if(err) return res.status(400).send("Hubo un error.");
 
         records.map(rec=>{
             rec.version = cargaVersion._id;
@@ -24,10 +38,10 @@ exports.crearCargaAcademica = async(req, res) => {
 
         await cargaAcademicaModel.insertMany(records)
         .then(()=>{
-            res.status(201).send("Se registro la carga academica exitosamente.");
+            res.status(201).send({message: "Se registro la carga academica exitosamente."});
         })
         .catch((err)=>{
-            res.status(401).send("No pudo registrar la carga academica.");
+            res.status(400).send("No pudo registrar la carga academica.");
             console.log(err)
         });
 
@@ -42,7 +56,7 @@ exports.eliminarCargaAcademica = async(req, res) => {
         res.status(201).send("Se elimino la carga academica exitosamente.")
     })
     .catch((err)=> {
-        res.status(401).send("No se pudo eliminar la carga academica.")
+        res.status(400).send("No se pudo eliminar la carga academica.")
     })
 }
 
@@ -57,6 +71,6 @@ exports.obtenerCargaAcademica = async(req, res) => {
     })
     .catch((err)=> {
         console.log(err);
-        res.status(401).send("No se pudieron obtener los registros de la carga.")
+        res.status(400).send("No se pudieron obtener los registros de la carga.")
     })        
 }
